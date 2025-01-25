0722e44e9f93aa4708c9398034bf3092bc8c0bdc
Pablo de Andres <pablodeandresolano@gmail.com>
2025-01-26 00:20:25 +0100

    Carrusel

--- script.js
+++ script.js
@@ -8,4 +8,22 @@ $(document).ready(function () {
 
     $(".chevron").toggleClass("rotated");
   });
+
+  let IndexActual = 0;
+
+  function updateCarrusel(index) {
+    $(".imagenes-carrusel img").removeClass("activo");
+
+    $(".imagenes-carrusel img").eq(index).addClass("activo");
+
+    $(".boton-carrusel").removeClass("activo");
+    $(".boton-carrusel").eq(index).addClass("activo");
+  }
+
+  $(".boton-carrusel").click(function () {
+    IndexActual = $(this).index();
+    updateCarrusel(IndexActual);
+  });
+
+  updateCarrusel(IndexActual);
 });
